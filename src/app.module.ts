c59a13fc16bbe624c70e4c788c974be46d675b43
Caio Fugii <caio.fugii@hotmail.com>
2025-05-21 14:41:20 -0300

    chore

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { BudgetsModule } from './budgets/budgets.module';
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
         url: config.get('DATABASE_URL'),
-        // ssl: { rejectUnauthorized: false },
+        ssl: { rejectUnauthorized: false },
         autoLoadEntities: true,
         synchronize: true, // cuidado: só em dev!
       }),
